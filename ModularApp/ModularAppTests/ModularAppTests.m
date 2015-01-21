0ccd401d22ed739c7025725f4bd81f86e7a801db
Hok Shun Poon <hok@hokshunpoon.me>
2015-01-21 16:12:12 +0000

    Hmm. Minor shifting of targets around seems to make tests want to run again.

---
+++ ModularApp/ModularAppTests/ModularAppTests.m
@@ -0,0 +1,40 @@
+//
+//  ModularAppTests.m
+//  ModularAppTests
+//
+//  Created by Hok Shun Poon on 21/01/2015.
+//  Copyright (c) 2015 Hok Shun Poon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ModularAppTests : XCTestCase
+
+@end
+
+@implementation ModularAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
